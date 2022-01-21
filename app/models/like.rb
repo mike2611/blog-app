2dd8f45c2ce5c36362e4d148e4a647c1b14b6866
mike2611 <mpuentes2611@gmail.com>
2022-01-21 17:18:54 -0600

    Add spec for like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_counter(post_id)
-    post = Post.find(post_id)
+  def update_likes_counter
     post.likes_counter += 1
     post.save
   end
