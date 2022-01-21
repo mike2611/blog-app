d73b3a5056c7ba73c97f1eba342b395ed9d73689
mike2611 <mpuentes2611@gmail.com>
2022-01-21 15:38:55 -0600

    Add spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = User.new
-  user.name = "Test"
-  user.photo = "https://thisisaurl.com"
-  user.bio = "This is the bio of Test user"
-  user.posts_counter = 0
-
-  post = Post.new
-  post.title = "This is a post"
-  post.text = "This is a test"
-  post.user_id = 1
-  post.comments_counter = 0
-  post.likes_counter = 0
-
-  before { user.save, post.save }
+  before(:each) { 
+    @user = User.new
+    @user.name = "Test"
+    @user.photo = "https://thisisaurl.com"
+    @user.bio = "This is the bio of Test user"
+    @user.posts_counter = 0 
+  }
 
   it 'name should be present' do
-    user.name = nil
-    expect(user).to_not be_valid
+    @user.name = nil
+    expect(@user).to_not be_valid
   end
 
   it 'posts_counter should >= 0' do
-    user.recent_posts
-    
-    @u.posts_counter = nil
+    @user.posts_counter = -1
+    expect(@user).to_not be_valid
+  end
+
+  it 'posts_counter should be an integer' do
+    @user.posts_counter = 'zero'
+    expect(@user).to_not be_valid
+  end
+
+  it 'show the 3 most recent post' do
+    @user.posts.new(title: 'test1', text: 'test', comments_counter: 0, likes_counter: 0)
+    @user.posts.new(title: 'test2', text: 'test', comments_counter: 0, likes_counter: 0)
+    @user.posts.new(title: 'test3', text: 'test', comments_counter: 0, likes_counter: 0)
+    @user.save
+
+    expect(@user.recent_posts[0].title).to eq 'test3'
+    expect(@user.recent_posts[1].title).to eq 'test2'
+    expect(@user.recent_posts[2].title).to eq 'test1'
   end
 
 end
